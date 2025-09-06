561479f7d4431b23e350e3beea6b6aaff03a9dbf
Muzamil Shamsi <muzamil.shamsi@outlook.com>
2025-09-06 18:40:22 -0400

    Flashcards html

--- obj/Debug/net9.0/MyWebApp.AssemblyInfo.cs
+++ obj/Debug/net9.0/MyWebApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyWebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61600563c129bcd7e1251e04719c8c16965b04d4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7751fb4204e76f13d060ae197c7aed16c2b984e8")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyWebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyWebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
